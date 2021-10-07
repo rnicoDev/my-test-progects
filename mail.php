720c246c416c9bf3bd834e5e86a8b5d8d20bed2b
Richie Nico <69207231+rnicoDev@users.noreply.github.com>
2021-10-06 23:58:50 -0400

    a

--- mail.php
+++ mail.php
@@ -29,6 +29,7 @@ $mail->addAddress('rnico.dev@gmail.com', 'John Doe');
 $mail->Subject = 'PHPMailer mail() test';
 //Read an HTML message body from an external file, convert referenced images to embedded,
 //convert HTML into a basic plain-text alternative body
+$mail->Body = 'Hello, this is my message.';
 $mail->msgHTML(file_get_contents('contents.html'), dirname(__FILE__));
 //Replace the plain text body with one created manually
 $mail->AltBody = 'This is a plain-text message body';
