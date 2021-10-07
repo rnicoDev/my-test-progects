99e98978db9992d4acf9b2c0dc40b199c74cff95
Richie Nico <69207231+rnicoDev@users.noreply.github.com>
2021-10-06 20:21:07 -0400

    a

--- mail.php
+++ mail.php
@@ -1,21 +1,12 @@
-<?
-$mail_to_send_to = "rnico.dev@gmail";
-$from_email = "admin@rnico.dev";
-$sendflag = $_REQUEST['sendflag'];    
-$name=$_REQUEST['name'];
-if ( $sendflag == "send" )
-        {
-                $subject= "Message subject";
-                $email = $_REQUEST['email'] ;
-                $message= "\r\n" . "Name: $name" . "\r\n"; //get recipient name in contact form
-                $message = $message.$_REQUEST['message'] . "\r\n" ;//add message from the contact form to existing message(name of the client)
-                $headers = "From: $from_email" . "\r\n" . "Reply-To: $email"  ;
-                $a = mail( $mail_to_send_to, $subject, $message, $headers );
-                if ($a)
-                {
-                     print("Message was sent, you can send another one");
-                } else {
-                     print("Message wasn't sent, please check that you have changed emails in the bottom");
-                }
-        }
-        ?>
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+$formcontent=" From: $name \n Phone: $phone \n Message: $message";
+$recipient = "rnico.dev@gmail.COM";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!" . " -" . "<a href='form.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
+?>
